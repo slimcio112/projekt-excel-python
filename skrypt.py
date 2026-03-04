4559fb0dac49d4cfaefd9c745a2a79c31e5c0ab1
Stanisław Kamzol <stanislaw.kamzol01@gmail.com>
2026-03-04 21:52:37 +0100

    add comments

--- skrypt.py
+++ skrypt.py
@@ -10,33 +10,31 @@ df_polisy['region'] = df_polisy['region'].str.capitalize()
 df_polisy['region'] = df_polisy['region'].fillna('brak danych')
 df_szkody['kwota_wyplacona_pln']= df_szkody['kwota_wyplacona_pln'].fillna(0)
 
-szkody_zsumowane = df_szkody.groupby('id_klienta')['kwota_wyplacona_pln'].sum().reset_index() #agregujemy szkody tak aby każdy klient pojawiał się w tabeli tylko 1 raz
+szkody_zsumowane = df_szkody.groupby('id_klienta')['kwota_wyplacona_pln'].sum().reset_index() # agregujemy szkody tak aby każdy klient pojawiał się w tabeli tylko 1 raz
 
 df_master = pd.merge(df_polisy, szkody_zsumowane, on='id_klienta', how='left') # łączymy tabele
 
-df_master['kwota_wyplacona_pln'] = df_master['kwota_wyplacona_pln'].fillna(0)
+df_master['kwota_wyplacona_pln'] = df_master['kwota_wyplacona_pln'].fillna(0) # zastępujemy puste dane zerami
 
-print(df_master)
-
+# Tworzymy raport, sumujemy skladki i wyplacone kwoty
 raport_regiony = df_master.groupby('region').agg({
     'skladka_pln': 'sum',
     'kwota_wyplacona_pln': 'sum'
 }).reset_index()
 
-raport_regiony['szkodowosc_%'] = ((raport_regiony['kwota_wyplacona_pln'] / raport_regiony['skladka_pln']) * 100).round(2)
+raport_regiony['szkodowosc_%'] = ((raport_regiony['kwota_wyplacona_pln'] / raport_regiony['skladka_pln']) * 100).round(2) # obliczamy wskaźnik szkodowości
 
-raport_regiony = raport_regiony.sort_values(by='szkodowosc_%', ascending=False)
+raport_regiony = raport_regiony.sort_values(by='szkodowosc_%', ascending=False) # sortujemy dane
 
 print("raport:")
 print(raport_regiony)
 
-raport_regiony.to_excel('raport.xlsx', index=False)
+raport_regiony.to_excel('raport.xlsx', index=False) # generujemy plik excel
 
+#rysujemy wykres
 plt.figure(figsize=(10,6))
-
 plt.bar(raport_regiony['region'], raport_regiony['szkodowosc_%'], color='skyblue', edgecolor='black')
 plt.axhline(y=100, color='red', linestyle='--', label='Próg straty')
-
 plt.title('wskaźnik straty dla każdego regionu')
 plt.xlabel('region')
 plt.ylabel('szkodowosc %')
